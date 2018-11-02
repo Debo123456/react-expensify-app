dabd73b24073e37a5b85fa5ec6c8187afe8c3bd6
Daniel <danieldawson496@gmail.com>
2018-11-01 23:25:25 -0500

    reverted expenselistitem

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import {removeExpense}from '../actions/expenses.js';
 import {connect} from 'react-redux';
-import moment from 'moment';
-import numeral from 'numeral';
 
 const ExpenseListItem = ({id, description, amount, createdAt, dispatch}) => (
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>{ numeral(amount).format('$0.00') } - { moment(createdAt).format('MMMM Do, YYYY') }</p>
+        <p>{ amount } - { createdAt }</p>
         <button onClick={() => {
             dispatch(removeExpense({ id: id }));
         }}>Remove</button>
