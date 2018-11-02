b2f5a14c8754bc389c5b85b6b76676442b6045b7
Daniel <danieldawson496@gmail.com>
2018-11-01 22:49:11 -0500

    Fixed createdAt format

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -11,9 +11,9 @@ const ExpenseListItem = ({id, description, amount, createdAt, dispatch}) => (
             <h3>{description}</h3>
         </Link>
         <p>
-            { numeral(amount / 100).format('$0,0.00') } 
+            { numeral(amount).format('$0.00') } 
             - 
-            { moment(createdAt).format('MMM Do, YYYY') }
+            { moment(createdAt).format('MMMM Do, YYYY') }
         </p>
         <button onClick={() => {
             dispatch(removeExpense({ id: id }));
